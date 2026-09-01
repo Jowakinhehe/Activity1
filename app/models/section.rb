91bedff352ae5f662047ef3d90140abb5d8b8dda
Viel Jaquin Lita <vieljoaqlita@gmail.com>
2026-09-01 15:01:46 +0800

    [issue/1] Display list of subjects in students/show

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,5 +1,5 @@
 class Section < ApplicationRecord
   belongs_to :subject
   has_many :classlists
-  has_many :students, through: :classlists
+  has_many :students, through: :classlists, dependent: :destroy
 end
